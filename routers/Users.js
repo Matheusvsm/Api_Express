d1e774f9780a1b48c1f3e3f1f0f4007240561f74
matheusvsm <matheusmoura.eng.eletrica@gmail.com>
2023-07-17 10:33:01 -0300

    corrigindo cors

--- routers/Users.js
+++ routers/Users.js
@@ -1,11 +1,7 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-router.get('/users', (req, res) => {
+router.get('/', (req, res) => {
   const users = [
     { id: 1, nome: 'Usuário 1', email: 'usuario1@example.com' },
     { id: 2, nome: 'Usuário 2', email: 'usuario2@example.com' },
@@ -13,30 +9,26 @@ router.get('/users', (req, res) => {
   res.json(users);
 });
 
-router.get('/users/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   const userId = req.params.id;
   const user = { id: userId, nome: 'Usuário', email: `usuario${userId}@example.com` };
   res.json(user);
 });
 
-router.post('/users', (req, res) => {
+router.post('/', (req, res) => {
   const newUser = req.body;
   res.json(newUser);
 });
 
-router.put('/users/:id', (req, res) => {
+router.put('/:id', (req, res) => {
   const userId = req.params.id;
   const updatedUser = req.body;
   res.json(updatedUser);
 });
 
-router.delete('/users/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   const userId = req.params.id;
   res.json({ message: 'Usuário excluído com sucesso.' });
 });
 
-app.use('/api', router);
-
-app.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const usersRouter = require('./routers/Users');
+const usersRouter = require('./routers/users');
 
 const app = express();
 
